cef79753702aee0a5a7b44b9f75cfbb9bd1de117
Steven Wichers <me@stevenwichers.com>
2021-04-26 15:57:49 -0700

    Add a filesystem cache.

--- src/GitHub/GitHubClientFactory.php
+++ src/GitHub/GitHubClientFactory.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace App\GitHub;
 
+use Cache\Adapter\Filesystem\FilesystemCachePool;
 use Github\Client;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
 
 /**
  * Class GitHubClientFactory.
@@ -20,7 +23,12 @@ class GitHubClientFactory {
    */
   public static function get(string $apiKey): Client {
 
+    $adapter = new Local(__DIR__ . '/../../');
+    $fs = new Filesystem($adapter);
+    $pool = new FilesystemCachePool($fs);
+
     $client = new Client();
+    $client->addCache($pool);
 
     $client->authenticate($apiKey, NULL, Client::AUTH_ACCESS_TOKEN);
 
